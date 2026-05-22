9e48e07b2d1ddf6febf2e1a063603b512b461ad1
ttiwari13 <ttanishka834@gmail.com>
2026-05-22 16:27:18 +0530

    added the code-editor and question editor logic

--- js/socket.js
+++ js/socket.js
@@ -1,13 +1,46 @@
 const socket=new WebSocket('ws://localhost:8080'); // stores live connection object
-const editor=document.getElementById('editor');
+const questionEdit=document.getElementById('question');
+const codeEdit=document.getElementById('code');
+const role='candidate';
 socket.onopen=()=>{
     console.log('Connected to the server');
 
 };
-editor.addEventListener('input',()=>{
-       socket.send(editor.value);
-});
+let lastsent=0;
+const delay=500;
+if(role==='candidate'){
+    questionEdit.disabled=true;
+    codeEdit.addEventListener('input',()=>{
+       const now=Date.now();
+       if(now-lastsent>=delay){
+          socket.send(JSON.stringify({
+            type:'code',
+            value:codeEdit.value
+         }));
+         lastsent=now;
+       }
+    });
+}
+if(role==='interviewer'){
+    codeEdit.disabled=true;
+    questionEdit.addEventListener('input',()=>{
+       const now = Date.now();
+       if(now-lastsent>=delay){
+          socket.send(JSON.stringify({
+            type: 'question',
+            value: questionEdit.value
+         }));
+         lastsent=now;
+       }
+    });
+}
 socket.onmessage=(event)=>{
-    editor.value=event.data; 
+    const data=JSON.parse(event.data);
+    if(data.type==='code'){
+        codeEdit.value=data.value;
+    }
+    if(data.type==='question'){
+        questionEdit.value=data.value;
+    }
     console.log('Message from server:',event.data);
 };

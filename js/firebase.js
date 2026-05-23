62bf8d6654729421bf618134ce82a07a98bb21b0
ttiwari13 <ttanishka834@gmail.com>
2026-05-23 23:04:00 +0530

    linked firebase

---
+++ js/firebase.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyAbhmKfJD4OjruASFCR5WfeQhmXnFhbTDU",
+  authDomain: "code-buddies-ed771.firebaseapp.com",
+  projectId: "code-buddies-ed771",
+  storageBucket: "code-buddies-ed771.firebasestorage.app",
+  messagingSenderId: "744567227794",
+  appId: "1:744567227794:web:26f3191b77092f1df2ed4b"
+};
+const app = initializeApp(firebaseConfig);
+export default app;
